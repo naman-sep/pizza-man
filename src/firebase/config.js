e3df24857490690df15dae340dfdcf71c5ea2db7
naman-sep <96712256+naman-sep@users.noreply.github.com>
2022-06-24 22:58:43 +0530

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+	apiKey: "API-KEY",
+	authDomain: "AUTH-DOMAIN.firebaseapp.com",
+	databaseURL: "DATABASE-URL.firebaseio.com",
+	projectId: "PROJECT-ID",
+	storageBucket: "STORAGE-BUCKET.appspot.com",
+	messagingSenderId: "MESSAGING-SENDER-ID",
+	appId: "APP-ID",
+	measurementId: "MEASUREMENT-ID",
+};
+
+export default firebaseConfig;
